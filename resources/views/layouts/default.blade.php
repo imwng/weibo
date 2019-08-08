91e504cc662b46f39d21906b4cd36094e7699d70
yoyh <im.wangxingxing@gmail.com>
2019-08-08 16:33:02 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,21 @@
 <!DOCTYPE html>
 <head>
 	<title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-	@yield('content')
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a href="/" class="navbar-brand">Webo App</a>
+			<ul class="navbar-nav justify-content-end">
+				<li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+				<li class="nav-item"><a href="#" class="nav-link">登陆</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<div class="container">
+		@yield('content')
+	</div>
 </body>
 </html>
