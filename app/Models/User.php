0c944df6cfb05cd62528a3bb320144dc31f16a2e
yoyh <im.wangxingxing@gmail.com>
2019-08-09 07:55:46 +0000

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,17 @@ class User extends Authenticatable
     ];
 
     protected $table = 'users';
+
+    /**
+     * gravatar 通用头像
+     * 1. 在gravatar网站使用email注册账号
+     * 2. 上传自己的头像
+     * 3. 根据gravatar约定的规则获取头像
+     * 4. 外部引用：$user->gravatar()
+     */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,12 @@ Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('/signup', 'UsersController@create')->name('signup');
+
+// Route::resource('users', 'UsersController');
+
+// 隐性路由模型绑定的好处：
+// get 匹配 userId，内部会多做一个操作，根据 id 获取对应模型 User，然后将 User 传递给 UsersController下的show
+// 开启条件：
+// 1. 传参变量必须是模型单数形式
+// 2. 控制器方法里要显式声明模型类型 public function show(User $user) {}
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
