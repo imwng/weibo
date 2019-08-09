c3ad2629ae5c2fd67dd48dbcf7454313fe58c463
yoyh <im.wangxingxing@gmail.com>
2019-08-09 08:56:39 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,10 +19,22 @@ class UsersController extends Controller
 
     public function store(Request $request)
     {
+        // 验证: 验证失败会直接return
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
+
+        // 存储
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+
+        // 将注册成功提示存入闪存
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
