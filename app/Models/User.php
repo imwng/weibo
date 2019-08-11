241cce3f0d9be8d9c3b399d93228930cbba5cd49
yoyh <im.wangxingxing@gmail.com>
2019-08-11 17:24:28 +0000

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,9 +67,15 @@ class User extends Authenticatable
         return $this->hasMany(Status::class);
     }
 
+    /**
+     * 动态流，主页显示微博动态
+     */
     public function feed()
     {
-        return $this->statuses()->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)->with('user')->orderBy('created_at', 'desc');
+        // return $this->statuses()->orderBy('created_at', 'desc');
     }
 
     public function followers()
